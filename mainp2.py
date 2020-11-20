2611faef5bcb8a250ac3a1d30147a8079a98073a
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-11-19 23:12:11 -0700

    added pymongo intialization command

--- mainp2.py
+++ mainp2.py
@@ -2,6 +2,13 @@
 
 import pymongo
 
+port = input("please enter the port you'd like to run the database on: ")
+client = pymongo.MongoClient("localhost", 27017)
+db_name = "291db"
+db = client.db_name
+print(db.name)
+
+
 
 def postQuestion():
     continue
@@ -17,3 +24,6 @@ def listAnswers():
 
 def AddVote():
     continue
+
+def main():
+    continue 
