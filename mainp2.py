73ae1225fa01fce42b1ceda60ef1fa9aee6e8d30
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-11-19 22:46:58 -0700

    added pymongo dependency and function names

---
+++ mainp2.py
@@ -0,0 +1,19 @@
+## mini project 2, CMPUT291
+
+import pymongo
+
+
+def postQuestion():
+    continue
+    
+def searchQuestion():
+    continue
+
+def answerQuestion():
+    continue
+    
+def listAnswers():
+    continue
+
+def AddVote():
+    continue
