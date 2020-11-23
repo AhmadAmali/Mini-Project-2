7748f64b8f61dd1bc2023b74db3b8e44642fe349
jegrywacheski <73365827+jegrywacheski@users.noreply.github.com>
2020-11-23 12:40:30 -0600

    Python file for phase 1

---
+++ mp2-2.py
@@ -0,0 +1,41 @@
+## mini project 2 Phase 1, CMPUT291
+
+import pymongo
+
+def createCollections():
+	#check if collections exist and drop if they do
+	colList = db.list_collection_names()
+	if "Posts" in colList:
+		collection = db["Posts"]
+		collection.drop()
+	if "Tags" in colList:
+		collection = db["Tags"]
+		collection.drop()
+	if "Votes" in colList:
+		collection = db["Votes"]
+		collection.drop()
+	#create new collections
+	posts = db["Posts"]
+	tags = db["Tags"]
+	votes = db["Customers"]
+	
+	#read json files and insert them into collections
+	with open('Posts.json') as file:
+		file_data = json.load(file)
+	posts.insert_many(file_data)
+	with open('Tags.json') as file:
+		file_data = json.load(file)
+	tags.insert_many(file_data)
+	with open('Votes.json') as file:
+		file_data = json.load(file)
+	votes.insert_many(file_data)
+
+
+def main():
+	port = input("Please enter the port you'd like to run the database on: ")
+	client = pymongo.MongoClient("localhost", 27017)
+	db = client['291db']	
+	createCollections()
+	
+if __name__ == "__main__":
+	main()
